f44c8653f842976ff57914a04451dc6c56702235
Vignesh Ravichandran <vignesh.ravichandran@ticketmaster.com>
2017-06-25 09:08:20 -0700

    customerId -> id everywhere

--- app/customerCreation.js
+++ app/customerCreation.js
@@ -1,13 +1,13 @@
 var gateway = require('./gateway');
 
-customerCreation = (firstName, lastName, email, customerId, req, callback) => {
+customerCreation = (firstName, lastName, email, id, req, callback) => {
   console.log('firstName: ' + firstName);
   console.log('lastName: ' + lastName);
   gateway.customer.create({
     firstName: firstName,
     lastName: lastName,
     email : email,
-    id : customerId
+    id : id
   }, function (err, result) {
     if (err) {
       console.log('error!');
